40f386a92b40123bd5d9dcfba64235779a92519c
tr4nt0r <manni@zapto.de>
2018-07-03 17:23:13 +0200

    changed from LCD to TFT, added Bootup Scrreen

--- Firmware/FuncGen.ino
+++ Firmware/FuncGen.ino
@@ -1,36 +1,32 @@
-
+#include <gfxfont.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
 #include "definitions.h"
-#include "LiquidCrystal_I2C.h"
 #include "AD9833.h"
 #include "Rotary.h"
-#include "math.h"
 
 
-AD9833 sigGen(10, 24000000); // Initialise our AD9833 with FSYNC pin = 10 and a master clock frequency of 24MHz
+
+
+AD9833 sigGen(AD9833_FsyncPin, 24000000); // Initialise our AD9833 with FSYNC pin = 10 and a master clock frequency of 24MHz
 LiquidCrystal_I2C lcd(0x27, 16, 2); // LCD Initialise
-Rotary encoder(2, 3);// Initialise the encoder on pins 2 and 3 (interrupt pins)
-
+Rotary encoder(rotEncPinA, rotEncPinB);// Initialise the encoder on pins 2 and 3 (interrupt pins)
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 void setup() {
-  // Initialise the LCD, start the backlight and print a "bootup" message for two seconds
-  lcd.begin();
-  lcd.backlight();
-  lcd.createChar(0, phi); // Custom PHI char for LCD
-  lcd.home();
-  lcd.print("AllAboutCircuits");
-  lcd.setCursor(0, 1);
-  lcd.print("Signal Generator");
-  delay(2000);
-  // Display initial set values for freq, phase, mode and power
-  lcd.clear();
-  displayFrequency();
-  displayMode();
+	Serial.begin(115200);
+	initTFT();
+	printBootUpMsg();
+	displayFrequency();
+	displayMode();
 #ifdef usePhase
-  displayPhase();
+	displayPhase();
 #endif
-  displayPower();
+	displayPower();
 #ifndef usePhase
-  displayFreqRegister();
+	displayFreqRegister();
 #endif
   // Initialise the AD9833 with 1KHz sine output, no phase shift for both
   // registers and remain on the FREQ0 register
@@ -55,7 +51,7 @@ void setup() {
   lastButtonState = 1;
   button = 1;
   // Set Cursor to initial possition
-  lcd.setCursor(0, 0);
+  //lcd.setCursor(0, 0);
 }
 
 void loop() {
@@ -85,11 +81,11 @@ void loop() {
   switch (menuState) {
     // Default state
     case 0: {
-        lcd.noBlink();
+        //lcd.noBlink();
         if (button == 0) {
           button = 1;
-          lcd.setCursor(0, 0);
-          lcd.blink();
+          //lcd.setCursor(0, 0);
+          //lcd.blink();
           menuState = 1;
           cursorPos = 0;
         }
@@ -138,7 +134,7 @@ void loop() {
             realPosC = settingsPos[cursorPos] - 16;
             realPosR = 1;
           }
-          lcd.setCursor(realPosC, realPosR);
+          //lcd.setCursor(realPosC, realPosR);
           lastCursorPos = cursorPos;
         }
       } break;
@@ -161,7 +157,7 @@ void loop() {
           menuState = 0;
         }
         // Set the blinking cursor on the digit you can currently modify
-        lcd.setCursor(9 - digitPos, 0);
+        //lcd.setCursor(9 - digitPos, 0);
       } break;
 
     // Phase setting
@@ -176,7 +172,7 @@ void loop() {
             sigGen.setPhase(phase);
           }
         }
-        lcd.setCursor(5 - digitPos, 1);
+        //lcd.setCursor(5 - digitPos, 1);
       } break;
     // Change the mode (sine/triangle/clock)
     case 5: {
@@ -185,7 +181,7 @@ void loop() {
           menuState = 0;
           sigGen.mode(currentMode);
         }
-        lcd.setCursor(13 ,1);
+        //lcd.setCursor(13 ,1);
       } break;
     // Just in case we messed something up
     default: {
@@ -230,7 +226,7 @@ void encChange() {
           // frequency, if all is good, make the change
           unsigned long newFrequency = frequency + pow(10, digitPos);
           unsigned char dispDigit =
-            frequency % pow(10, digitPos + 1) / pow(10, digitPos);
+            frequency % (unsigned long) pow(10, digitPos + 1) / pow(10, digitPos);
           if (newFrequency <= maxFrequency && dispDigit < 9) {
             frequency += pow(10, digitPos);
             updateDisplay = true;
@@ -281,7 +277,7 @@ void encChange() {
 
       case 2: {
           unsigned long newFrequency = frequency + pow(10, digitPos);
-          unsigned char dispDigit = frequency % pow(10, digitPos + 1) / pow(10, digitPos);
+          unsigned char dispDigit = frequency % (unsigned long) pow(10, digitPos + 1) / pow(10, digitPos);
 
           if (newFrequency > 0 && dispDigit > 0) {
             frequency -= pow(10, digitPos);
@@ -324,45 +320,129 @@ void encChange() {
 // Function to display the current frequency in the top left corner
 void displayFrequency() {
   unsigned long frequencyToDisplay = frequency;
-  lcd.setCursor(0, 0);
-  lcd.print("f=");
-  for (int i = 7; i >= 0; i--) {
-    unsigned char dispDigit = frequencyToDisplay / pow(10, i);
-    lcd.print(dispDigit);
-    frequencyToDisplay -= dispDigit * pow(10, i);
+  
+  tft.setCursor(0, 0);
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(3);
+  tft.print(F("F="));
+
+  if (frequency >= 1000000) {
+	  tft.print(frequency / 1000000.0, 4);
+	  tft.print(F(" GHz"));
+  } else if (frequency >= 1000) {
+	  tft.print(frequency / 1000.0, 3);
+	  tft.print(F(" MHz"));
+  } else {
+	  tft.print(frequency, 0);
+	  tft.print(F(" Hz"));
   }
-  lcd.print("Hz");
+ 
 }
 // Function to display power state (ON/OFF) in the top right corner
 void displayPower() {
-  lcd.setCursor(13, 0);
-  lcd.print(powerState[currentPowerState]);
+	tft.setCursor(0, 30);
+	tft.setTextColor(ILI9341_WHITE);
+	tft.setTextSize(3);
+	tft.print(powerState[currentPowerState]);
 }
 // Function to display the mode in the bottom right corner
 void displayMode() {
-  lcd.setCursor(13, 1);
-  lcd.print(mode[currentMode]);
+	tft.setTextColor(ILI9341_WHITE);
+	tft.setTextSize(3);
+	tft.setCursor(0, 60);
+	tft.print(mode[currentMode]);
 }
 // Function to display the mode in the bottom left corner
 // Only used if you enable PHASE setting instead of FREQ register
 void displayPhase() {
   unsigned int phaseToDisplay = phase;
-  lcd.setCursor(0, 1);
-  lcd.write(0);
-  lcd.print("=");
+  //lcd.setCursor(0, 1);
+  //lcd.write(0);
+  //lcd.print("=");
   for (int i = 3; i >= 0; i--) {
     unsigned int dispDigit = phaseToDisplay / pow(10, i);
-    lcd.print(dispDigit);
+    //lcd.print(dispDigit);
     phaseToDisplay -= dispDigit * pow(10, i);
   }
 }
 // Function to display the FREQ register (either 0 or 1) in the bottom left
 // corner
 void displayFreqRegister() {
-  lcd.setCursor(0, 1);
-  lcd.print("FREQ");
-  lcd.print(freqRegister);
+	tft.setCursor(0, 90);
+	tft.setTextSize(3);
+	tft.setTextColor(ILI9341_WHITE);
+	tft.print("Preset ");
+	tft.print(freqRegister);
+	
+	//lcd.setCursor(0, 1);
+  //lcd.print("FREQ");
+  //lcd.print(freqRegister);
 }
 
 
 
+void initTFT() {
+
+	tft.begin();
+
+	// read diagnostics (optional but can help debug problems)
+	uint8_t x = tft.readcommand8(ILI9341_RDMODE);
+	Serial.print(F("Display Power Mode: 0x")); Serial.println(x, HEX);
+	x = tft.readcommand8(ILI9341_RDMADCTL);
+	Serial.print(F("MADCTL Mode: 0x")); Serial.println(x, HEX);
+	x = tft.readcommand8(ILI9341_RDPIXFMT);
+	Serial.print(F("Pixel Format: 0x")); Serial.println(x, HEX);
+	x = tft.readcommand8(ILI9341_RDIMGFMT);
+	Serial.print(F("Image Format: 0x")); Serial.println(x, HEX);
+	x = tft.readcommand8(ILI9341_RDSELFDIAG);
+	Serial.print(F("Self Diagnostic: 0x")); Serial.println(x, HEX);
+	Serial.println(F("Done!"));
+
+	tft.setRotation(3);
+
+	tft.fillScreen(PINK);
+}
+
+void printBootUpMsg() {
+	tft.fillScreen(PINK);
+	tft.drawRect(0, 0, tft.width(), tft.height() / 5, ILI9341_BLACK);
+	tft.fillRect(0, 0, tft.width(), tft.height() / 5, BLUE);
+
+	tft.setTextColor(PINK);
+
+	printAlignCenter(F("Signal Generator"), 3, tft.width() / 2, tft.height() / 10);
+	tft.drawBitmap(tft.width() / 2 - 48, tft.height() / 2 -19, sappz, 96, 38, PINK, ILI9341_BLACK);
+	tft.setTextColor(ILI9341_BLACK);
+
+	printAlignCenter(_SIGGEN_VERSION_, 2, tft.width()/2, 200);
+	printAlignLeft(F("Powered by"), 1, tft.width() / 2 - 48 - 5, tft.height() / 2 + 10);
+	delay(2000);
+	tft.fillScreen(ILI9341_BLACK);
+}
+
+
+void printAlignCenter(const String text, uint8_t s, uint16_t x, uint16_t y) {
+	tft.setCursor(x - text.length() * 3 * s, y - 4 * s);
+	tft.setTextSize(s);
+	tft.print(text);
+}
+
+void printAlignLeft(const String text, uint8_t s, uint16_t x, uint16_t y) {
+	tft.setCursor(x - text.length() * 6 * s, y - 4 * s);
+	tft.setTextSize(s);
+	tft.print(text);
+}
+
+int pow(int base, int exp) {
+	int result = 1;
+	for (;;) {
+		if (exp & 1)
+			result *= base;
+		exp >>= 1;
+		if (!exp)
+			break;
+		base *= base;
+	}
+
+	return result;
+}
